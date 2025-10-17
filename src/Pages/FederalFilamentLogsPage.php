4288c87a1e9aedad37996cf9749e3f97bc93d7f2
Alexandre Ferreira <shieldforce2@gmail.com>
2025-10-16 21:54:27 -0300

    start

--- src/Pages/FederalFilamentLogsPage.php
+++ src/Pages/FederalFilamentLogsPage.php
@@ -2,25 +2,10 @@
 
 namespace Shieldforce\FederalFilamentLog\Pages;
 
-use Carbon\Carbon;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Concerns\InteractsWithTable;
-use Filament\Tables\Contracts\HasTable;
-use Filament\Tables\Filters\SelectFilter;
-use Filament\Tables\Filters\Filter;
-use Filament\Tables\Table;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
 
-class FederalFilamentLogsPage extends Page implements HasForms, HasTable
+class FederalFilamentLogsPage extends Page
 {
-    use InteractsWithForms;
-    use InteractsWithTable;
-
     protected static string  $view            = 'federal-filament-log::pages.index';
     protected static ?string $navigationIcon  = 'heroicon-o-list-bullet';
     protected static ?string $navigationGroup = 'Logs';
